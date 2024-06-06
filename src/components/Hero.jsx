323ba527eb5253c7a39e389377099194041d28fa
Pfarelo Nelwamondo <Pfare5057@gmail.com>
2024-06-06 02:39:31 +0200

    Adding content on the Hero page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sum_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/pfare-nel/Ai_summarizer")
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Hero;
